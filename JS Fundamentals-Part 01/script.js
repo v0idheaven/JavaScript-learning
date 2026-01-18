f40d7e63328f5a33af00cf942ad58ca8eb20242f
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 19:06:19 +0530

    Type conversion and coercion

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -199,3 +199,25 @@ if(BMIMark2 > BMIJohn2){
 } else{
     console.log("John's BMI is higher than Mark's!");
 }
+
+
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(inputYear + 18);
+
+console.log(Number("Varun"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am' + 23 + ' years old');
+console.log('I am' + String(23) + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
