8caa42a3d6042658e71f32d0a547de6cc01ef010
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 19:15:00 +0530

    truthy and falsy values

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -221,3 +221,28 @@ console.log('23' / '2');
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+
+
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Varun'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!')
+}
+
+
+let height = 0;
+if (height){
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED')
+}
