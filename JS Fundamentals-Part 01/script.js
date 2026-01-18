db712c29062ec2c129a9cf93aa4ed50583c07962
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 16:01:35 +0530

    values and variables

---
+++ JS Fundamentals-Part 01/script.js
@@ -0,0 +1,25 @@
+let js = "amazing"
+console.log(40 + 8 + 23 - 10)
+
+console.log("Varun")
+console.log(23)
+
+let firstName = "Varun";
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = "Varun";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob)
