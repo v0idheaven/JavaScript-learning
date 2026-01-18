593197ac8cd4ce38ff9f757e66257ded880e03a0
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 17:28:36 +0530

    Basic operators

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -62,3 +62,46 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = "Schmedtmann";
+console.log(lastName);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas - ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName2 = 'Jonas';
+const lastName2 = 'Schmedtmann';
+console.log(firstName2 + lastName2);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018)
