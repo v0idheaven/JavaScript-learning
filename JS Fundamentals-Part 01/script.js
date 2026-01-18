74751df31b278394f82ae6f7682595f865c173bc
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 18:27:40 +0530

    challenge 02

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -178,3 +178,24 @@ if(birthYear4 <= 2000) {
     let century = 21;
 }
 console.log(century);
+
+
+// const massMark2 = 78;
+// const heightMark2 = 1.69;
+// const massJohn2 = 92;
+// const heightJohn2 = 1.95;
+
+const massMark2 = 95;
+const heightMark2 = 1.;88
+const massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const BMIMark2 = massMark2 / heightMark2 ** 2;
+const BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+console.log(BMIMark2, BMIJohn2);
+
+if(BMIMark2 > BMIJohn2){
+    console.log("Mark's BMI is higher than John's!");
+} else{
+    console.log("John's BMI is higher than Mark's!");
+}
