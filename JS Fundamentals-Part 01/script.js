897520e2c9bba25a976e13b18acf13b55967dbcc
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 19:35:33 +0530

    equality operators

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -246,3 +246,24 @@ if (height){
 } else {
     console.log('Height is UNDEFINED')
 }
+
+
+
+const agge = 10;
+if (age === 18) console.log('You just become an adult :D (strict)');
+
+if (age == 18) console.log('You just become an adult :D (loose)');
+
+const favourite = Number(prompt("What's your favourite number!"))
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite == 23) {
+    console.log('Cool! 23 is an amzaing number!')
+} else if(favourite === 7){
+    console.log('7 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7')
+}
+
+if (favourite !== 23) console.log('Why not 23?')
