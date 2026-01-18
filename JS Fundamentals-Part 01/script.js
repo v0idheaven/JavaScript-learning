e2f48ee5f20c814df950ae148fde3086f22a450f
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-18 17:53:35 +0530

    challenge 01

--- JS Fundamentals-Part 01/script.js
+++ JS Fundamentals-Part 01/script.js
@@ -119,3 +119,20 @@ console.log(a, b);
 
 const averageAge = (ageJonas2 + ageSarah2) / 2
 console.log(ageJonas2, ageSarah2, averageAge)
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.;88
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
