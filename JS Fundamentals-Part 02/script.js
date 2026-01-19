c44b59bc9bd6576990bcec0546d2e0271ea712ed
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-19 18:59:38 +0530

    arrow function

--- JS Fundamentals-Part 02/script.js
+++ JS Fundamentals-Part 02/script.js
@@ -53,3 +53,21 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+
+///////////////////////////////////////
+// Arrow Function
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'))
+console.log(yearsUntilRetirement(1980, 'Bob'))
