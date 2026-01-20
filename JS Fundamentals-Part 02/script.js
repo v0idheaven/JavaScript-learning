e70c0ef7074884fef1f2a197a4d413b57adc75f7
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-20 17:45:09 +0530

    looping array, breaking and continue

--- JS Fundamentals-Part 02/script.js
+++ JS Fundamentals-Part 02/script.js
@@ -431,3 +431,56 @@ if (mark.bmi > john.bmi) {
 for (let rep = 1; rep <= 30; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
+
+
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const yearss = [1991, 2007, 1969, 2020];
+const agess = [];
+
+for (let i = 0; i < yearss.length; i++) {
+  agess.push(2037 - yearss[i]);
+}
+console.log(agess);
+
+// continue and break
+console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
