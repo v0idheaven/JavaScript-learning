8629299747c79b822f03c885d535451901f162f0
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-20 17:57:01 +0530

    looping backwards and loops in loops

--- JS Fundamentals-Part 02/script.js
+++ JS Fundamentals-Part 02/script.js
@@ -484,3 +484,30 @@ for (let i = 0; i < jonas.length; i++) {
 
   console.log(jonas[i], typeof jonas[i]);
 }
+
+
+///////////////////////////////////////
+// Looping Backwards and Loops in Loops
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
+}
