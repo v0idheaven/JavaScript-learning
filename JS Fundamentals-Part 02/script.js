236588b64631296220cd3ee8cf04e982312afd4f
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-20 11:18:05 +0530

    basic array operations

--- JS Fundamentals-Part 02/script.js
+++ JS Fundamentals-Part 02/script.js
@@ -201,3 +201,37 @@ console.log(age4, age5, age6);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
+
+
+///////////////////////////////////////
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add Elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove Elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23')); // False
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven.');
+}
