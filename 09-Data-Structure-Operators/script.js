3ae4618bba28f97c4a51c5936bd3833b88cd729c
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-23 23:53:53 +0530

    join 2 array or copy array

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -46,6 +46,23 @@ console.log(newArr);
 console.log(...newArr);
 console.log(1, 2, 7, 8, 9);
 
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menuu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menuu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = "Jonas";
+const letters = [...str, "", "S."];
+console.log(letters);
+console.log(...str);
+
+
 /////////////////////////////////
 // Destructuring objects
 restaurant.orderDelivery({
