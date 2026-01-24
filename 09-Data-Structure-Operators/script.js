4dce9b72cbd1e694eed9e45d31c543b43ffc0718
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-24 10:16:30 +0530

    rest pattern and parameters

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -34,7 +34,59 @@ const restaurant = {
       `Order recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+
+/////////////////////////////////
+// The Rest Pattern and Parameters
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const aarr = [1, 2, ...[3, 4]];
+console.log(aarr);
+
+// REST, because on LEFT side of =
+const [m, n, ...others] = [1, 2, 3, 4, 5];
+console.log(m, n, others);
+
+const [pizza, , risotto, ...othersFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, othersFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const numbers = [23, 5, 7];
+add(...numbers);
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
+
+
+/////////////////////////////////
+// The Spread Operator (...)
 
 const arrr = [7, 8, 9];
 const badNewArr = [1, 2, arrr[0], arrr[1], arrr[2]];
