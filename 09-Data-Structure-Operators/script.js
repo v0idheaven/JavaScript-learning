ca73b6dd62c72597edfc0865b64d6d19458f47f4
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-24 19:39:37 +0530

    logical assignment

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -57,6 +57,15 @@ console.log(rest1.numberGuests);
 rest2.numberGuests = rest2.numGuests || 10;
 console.log(rest2.numberGuests);
 
+// OR assignment operator
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+console.log(rest1);
+console.log(rest2);
 
 //////////////////////////////////
 // The Nullish Coalescing Operator
