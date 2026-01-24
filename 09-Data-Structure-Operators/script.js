4e5d40824702d3953f9b12c81945940349042f96
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-24 23:42:18 +0530

    logical assignment operators

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -64,6 +64,16 @@ rest2.numGuests = rest2.numGuests || 10;
 rest1.numGuests ||= 10;
 rest2.numGuests ||= 10;
 
+// Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
 console.log(rest1);
 console.log(rest2);
 
