148a9064044b323401ad1172866703212f94ecb8
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-25 11:08:56 +0530

    Enhanced object literals

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -1,25 +1,28 @@
 "strict-mode";
 
+const weekdayss = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHourss = {
+  [weekdayss[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdayss[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdayss[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -51,6 +54,14 @@ const rest2 = {
   name: "La Piazza",
   owner: "Giovanni Rossi",
 };
+
+const mmenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of mmenu) console.log(item);
+
+for (const [i, el] of mmenu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
 
 
 ///////////////////////////////////////
