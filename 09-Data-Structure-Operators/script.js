4c6fa735b2be70793078a97ce6b0b3644106fac4
v0idheaven <dahiyavarun2007@gmail.com>
2026-01-26 23:58:33 +0530

    coding challenge 2

--- 09-Data-Structure-Operators/script.js
+++ 09-Data-Structure-Operators/script.js
@@ -110,7 +110,15 @@ const gamee = {
       'Gotze',
     ],
   ],
-  
+    score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
 
 
